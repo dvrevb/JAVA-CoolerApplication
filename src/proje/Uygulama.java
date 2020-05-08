3ab21859fbaf662744322d9c081b55bd877115b3
dvrevb <dvrevb@outlook.com>
2020-05-08 23:43:31 +0300

    update

--- src/proje/Uygulama.java
+++ src/proje/Uygulama.java
@@ -1,10 +1,5 @@
 package proje;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class Uygulama {
     public static void main(String[] args) {
        AkilliCihaz cihaz=new AkilliCihaz();
